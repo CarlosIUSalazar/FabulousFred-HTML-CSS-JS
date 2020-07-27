24479f8e0a8f8351d9220e936c0cd5135e2349dd
Carlos Salazar <carlosiusalazar@gmail.com>
2020-07-27 23:10:44 +0900

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let buttonOne = document.getElementById("one");
+let buttonTwo = document.getElementById("two");
+let buttonThree = document.getElementById("three");
+let buttonFour = document.getElementById("four");
+let buttonFive = document.getElementById("five");
+let buttonSix = document.getElementById("six");
+let buttonSeven = document.getElementById("seven");
+let buttonEigth = document.getElementById("eigth");
+let buttonNine = document.getElementById("nine");
+
+alert("Hello")
