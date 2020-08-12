c921fcca161ad9a75f23db7b61423d8f981814a7
Carlos Salazar <carlosiusalazar@gmail.com>
2020-08-12 18:45:20 +0900

    Generate sequence implementation

--- script.js
+++ script.js
@@ -69,6 +69,7 @@ function generateSequence(){
         let randomNumber = Math.floor(Math.random() * Math.floor(9)) + 1;
         generatedSequence.push(randomNumber);
     }
+    //console.log("generatedSequence inside generateSequence()", generatedSequence)
     return generatedSequence;
     //console.log( "The sequence generated is", generatedSequence)
 }
@@ -76,56 +77,54 @@ function generateSequence(){
 function playGame(){
     round++;
     roundText.innerHTML = round;
-    setTimeout(function(){generateSequence()}, 1000);
+    generateSequence();
     console.log("generatedSequence", generatedSequence)
     for (let i = 0; i < generatedSequence.length; i++){
-        console.log("generatedSequnce",generatedSequence)
-        console.log(generatedSequence[i])
         if (generatedSequence[i] === 1){
             buttonOne.style.backgroundColor = "white"
             playAudioOne();
             setTimeout(function(){buttonOne.style.backgroundColor = "red"}, 300);
         }
-        if (generatedSequence[i] === 2){
-            buttonTwo.style.backgroundColor = "white"
-            playAudioTwo();
-            setTimeout(function(){buttonTwo.style.backgroundColor = "yellow"}, 300);
-        }
-        if (generatedSequence[i] === 3){
-            buttonThree.style.backgroundColor = "white"
-            playAudioThree();
-            setTimeout(function(){buttonThree.style.backgroundColor = "orange"}, 300);
-        }
-        if (generatedSequence[i] === 4){
-            buttonFour.style.backgroundColor = "white"
-            playAudioFour();
-            setTimeout(function(){buttonFour.style.backgroundColor = "greenyellow"}, 300);
-        }
-        if (generatedSequence[i] === 5){
-            buttonFive.style.backgroundColor = "white"
-            playAudioFive();
-            setTimeout(function(){buttonFive.style.backgroundColor = "violet"}, 300);
-        }
-        if (generatedSequence[i] === 6){
-            buttonSix.style.backgroundColor = "white"
-            playAudioSix();
-            setTimeout(function(){buttonSix.style.backgroundColor = "limegreen"}, 300);
-        }
-        if (generatedSequence[i] === 7){
-            buttonSeven.style.backgroundColor = "white"
-            playAudioSeven();
-            setTimeout(function(){buttonSeven.style.backgroundColor = "purple"}, 300);
-        }
-        if (generatedSequence[i] === 8){
-            buttonEight.style.backgroundColor = "white"
-            playAudioEight();
-            setTimeout(function(){buttonEight.style.backgroundColor = "cyan"}, 300);
-        }
-        if (generatedSequence[i] === 9){
-            buttonNine.style.backgroundColor = "white"
-            playAudioNine();
-            setTimeout(function(){buttonNine.style.backgroundColor = "blue"}, 300);
-        }
+        // if (generatedSequence[i] === 2){
+        //     buttonTwo.style.backgroundColor = "white"
+        //     playAudioTwo();
+        //     setTimeout(function(){buttonTwo.style.backgroundColor = "yellow"}, 300);
+        // }
+        // if (generatedSequence[i] === 3){
+        //     buttonThree.style.backgroundColor = "white"
+        //     playAudioThree();
+        //     setTimeout(function(){buttonThree.style.backgroundColor = "orange"}, 300);
+        // }
+        // if (generatedSequence[i] === 4){
+        //     buttonFour.style.backgroundColor = "white"
+        //     playAudioFour();
+        //     setTimeout(function(){buttonFour.style.backgroundColor = "greenyellow"}, 300);
+        // }
+        // if (generatedSequence[i] === 5){
+        //     buttonFive.style.backgroundColor = "white"
+        //     playAudioFive();
+        //     setTimeout(function(){buttonFive.style.backgroundColor = "violet"}, 300);
+        // }
+        // if (generatedSequence[i] === 6){
+        //     buttonSix.style.backgroundColor = "white"
+        //     playAudioSix();
+        //     setTimeout(function(){buttonSix.style.backgroundColor = "limegreen"}, 300);
+        // }
+        // if (generatedSequence[i] === 7){
+        //     buttonSeven.style.backgroundColor = "white"
+        //     playAudioSeven();
+        //     setTimeout(function(){buttonSeven.style.backgroundColor = "purple"}, 300);
+        // }
+        // if (generatedSequence[i] === 8){
+        //     buttonEight.style.backgroundColor = "white"
+        //     playAudioEight();
+        //     setTimeout(function(){buttonEight.style.backgroundColor = "cyan"}, 300);
+        // }
+        // if (generatedSequence[i] === 9){
+        //     buttonNine.style.backgroundColor = "white"
+        //     playAudioNine();
+        //     setTimeout(function(){buttonNine.style.backgroundColor = "blue"}, 300);
+        // }
     }
 }
 
